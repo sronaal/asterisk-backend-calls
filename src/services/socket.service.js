4717b41ed67e1fdab5e59a69222b79c00ae417e6
Ronal S. <ronal.sanx21@gmail.com>
2026-02-27 16:21:11 -0500

    Refactor servicio de socket: mejora en la estructura y limpieza del código

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -1,34 +1,39 @@
-const {Server} = require("socket.io")
-class SocketService{
+const { Server } = require("socket.io")
+class SocketService {
 
-    constructor(){
+    constructor() {
         this.io = null
     }
 
 
-    init(server){
-
+    init(server) {
         this.io = new Server(server, {
-            cors:{
+            cors: {
                 origin: '*',
                 methods: ['GET', 'POST']
             }
         });
-    
-        this.io.on('connection', (socket) => {
 
-            const estadoInicial = {
-                llamadasActivas: 0,
-                colasActivas: 0
-            }
+        // Namespace dedicado para monitoreo en tiempo real
+        this.realtime = this.io.of("/realtime");
 
-            socket.emit('state.initial', estadoInicial)
-        })
+        this.realtime.on('connection', (socket) => {
+            console.log('Cliente conectado al namespace /realtime');
 
-        
-
-
+            // Podríamos emitir el estado actual al conectar
+            socket.emit('auth:success', { connected: true });
+        });
     }
 
-    
+    /**
+     * Emite un evento a todos los clientes en el namespace /realtime
+     */
+    emit(event, data) {
+        if (this.realtime) {
+            this.realtime.emit(event, data);
+        }
+    }
 }
+
+const socketService = new SocketService();
+module.exports = socketService;

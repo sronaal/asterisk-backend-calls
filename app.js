638b0cdcee0b4a9d91395a2c038a158e3d2ab397
Ronal S. <ronal.sanx21@gmail.com>
2026-02-27 16:01:26 -0500

    Agregar soporte para CORS en la aplicación y actualizar dependencias en package.json y package-lock.json

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const http = require("http")
+const cors = require("cors")
 const conexionMysql = require('./src/db/mysql')
 const rutasCDR = require('./src/routes/routers.cdr')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/v1/llamadas', rutasCDR)
 
